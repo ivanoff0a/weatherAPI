fc8c496d6553073e916feb348b196a4ee90d5bdf
ivanoff0a <ivanov-andrey15@mail.ru>
2018-03-22 17:48:03 +0300

    v.2

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -6,10 +6,11 @@ $(document).ready(function (){
         degree = $('.weather-degree');
         btn = $('.js-btn');
         input = $('.js-input');
+        errorNotifier = $('.error-notif')
 
-    $.getJSON('http://api.openweathermap.org/data/2.5/weather?id=498817&APPID=24ded0daa1863e28a12f2962c113341b', function(result) {
-        setWeatherData(result);
-    })
+    // $.getJSON('http://api.openweathermap.org/data/2.5/weather?id=498817&APPID=24ded0daa1863e28a12f2962c113341b', function(result) {
+    //     setWeatherData(result);
+    // })
 
     function setWeatherData(result) {
         city.text(result.name);
@@ -18,10 +19,23 @@ $(document).ready(function (){
         desc.text(result.weather[0].description);
     }
 
+    function isError() {
+        errorNotifier.toggleClass('-activated');
+    }
+
+    function isNotACity() {
+        message == "city not found";
+    }
+
     btn.click(function() {
         $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + input.val() + '&APPID=24ded0daa1863e28a12f2962c113341b', function(result) {
             setWeatherData(result);
+            if (input.isNotACity()) {
+                return isError();
+            }
         });
     })
+
+
 });
 
